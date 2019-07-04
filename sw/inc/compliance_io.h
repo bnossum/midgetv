9d9319b1b30ddbfb6743315ae662436574296a08
bnossum <bnossum@gmail.com>
2019-07-04 15:37:08 +0200

    Add files via upload

---
+++ sw/inc/compliance_io.h
@@ -0,0 +1,38 @@
+#define BAREBONE 1
+#if BAREBONE
+#define RVTEST_IO_INIT
+#define RVTEST_IO_WRITE_STR(_STR)
+#define RVTEST_IO_CHECK()
+#define RVTEST_IO_ASSERT_GPR_EQ(_R, _I)
+#define RVTEST_IO_ASSERT_SFPR_EQ(_F, _R, _I)
+#define RVTEST_IO_ASSERT_DFPR_EQ(_D, _R, _I)
+#else
+Can't get this to work..
+#define RVTEST_IO_INIT
+#define RVTEST_IO_WRITE_STR(_STR) \
+        .section .rodata;         \
+1984:                             \
+        .string _STR;             \
+        .section .text;           \
+        la      a0,1984b;         \
+        jal     FN_WriteStr;
+
+//
+// FN_WriteStr
+//
+FN_WriteStr:
+        mv      t0,a0;
+1:      lbu     a0,(t1);
+        addi    t1,t1,1;
+        beq     a0,zero,1f;
+        simerr  31;
+        simputc 10;
+        j       1b;
+1:
+        ret;
+
+#define RVTEST_IO_CHECK()
+#define RVTEST_IO_ASSERT_GPR_EQ(_R, _I)
+#define RVTEST_IO_ASSERT_SFPR_EQ(_F, _R, _I)
+#define RVTEST_IO_ASSERT_DFPR_EQ(_D, _R, _I)
+#endif
---
+++ sw/inc/compliance_test.h
@@ -0,0 +1,20 @@
+#ifndef _COMPLIANCE_TEST_H
+#define _COMPLIANCE_TEST_H
+
+#define RV_COMPLIANCE_HALT\
+        j midgetv_dump_compliance
+        
+#define RV_COMPLIANCE_RV32M       
+#define RV_COMPLIANCE_CODE_BEGIN                \
+        _midgetstart:
+
+#define RV_COMPLIANCE_CODE_END    
+
+#define RV_COMPLIANCE_DATA_BEGIN                \
+        .align 4;                               \
+        midgetv_compliance_data_begin:          \
+
+#define RV_COMPLIANCE_DATA_END                  \
+        midgetv_compliance_data_end:
+
+#endif
