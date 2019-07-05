d863ae73dce60194171979847bfcc48d9e60f463
bnossum <bnossum@gmail.com>
2019-07-05 07:04:26 +0200

    Missed a pin

--- code/m_ice40sim_EBRonly.v
+++ code/m_ice40sim_EBRonly.v
@@ -26,6 +26,7 @@ module m_ice40sim_EBRonly
    wire   midgetv_core_killwarnings;// From inst_midgetv_core of m_midgetv_core.v
    wire                 CYC_O;                  // From inst_midgetv_core of m_midgetv_core.v
    wire [31:0]          dbgpc;                  // From inst_midgetv_core of m_midgetv_core.v
+   wire                 corerunning;            // From inst_midgetv_core of m_midgetv_core.v
    /* verilator lint_on UNUSED */
    
    /*AUTOWIRE*/
@@ -69,6 +70,7 @@ module m_ice40sim_EBRonly
       .ADR_O                            (ADR_O[31:0]),
       .DAT_O                            (DAT_O[31:0]),
       .SEL_O                            (SEL_O[3:0]),
+      .corerunning                      (corerunning),
       .dbgpc                            (dbgpc[31:0]),
       .midgetv_core_killwarnings        (midgetv_core_killwarnings),
       // Inputs
