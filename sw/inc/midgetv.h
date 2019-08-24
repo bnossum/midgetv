ac7db1366246929e1bf2a1e4bc207b9d58afcf2a
bnossum <bnossum@gmail.com>
2019-08-24 09:41:55 +0200

    m

---
+++ sw/inc/midgetv.h
@@ -0,0 +1,92 @@
+/* The start of EBR is taken up by registers.
+ * Then comes magic constants and system registers
+ * Originally 
+ *   volatile uint32_t x[32];
+ * was present in the start of SYSEBR_TypeDef, but then the mapping
+ *   #define SYSEBR ((SYSEBR_TypeDef *)0) led to disasters, probably some aliasing
+ * with NULL. The simplest consistent way to avoid the problem was to take out
+ * the registers, and do what amounts to 
+ *   #define SYSEBR ((SYSEBR_TypeDef *)0x80)
+ */
+typedef struct {
+        //const uint32_t        x0;    see comment above
+        //volatile uint32_t     x[31]; see comment above.
+        volatile uint32_t       jj;
+        volatile uint32_t       rinst;
+        const volatile uint32_t pc;
+        volatile uint32_t       mcycle;
+        uint32_t                rNMI_IIV;
+        const uint32_t          cteffffff7f;    
+        const uint32_t          cte000000ff;    
+        const uint32_t          cte0000ffff;    
+        const uint32_t          cteffff7fff;    
+        uint32_t                mtvec;        
+        const uint32_t          cte00000000;    
+        const uint32_t          cteffffffff;    
+        volatile uint32_t       yy;           
+        volatile uint32_t       mepc;
+        volatile uint32_t       mcause;       
+        volatile uint32_t       mtval;        
+        volatile uint32_t       mscratch;     
+        volatile uint32_t       misa;         
+        volatile uint32_t       ex_dup_x0;     
+        volatile uint32_t       ex_rCSRadr;    
+        volatile uint32_t       ex_rCSRty;     
+        volatile uint32_t       ex_rrs1;       
+        volatile uint32_t       ex_rrd;        
+        volatile uint32_t       ex_rtmp;       
+        const volatile uint32_t mtime;         // Write adr is different
+        volatile uint32_t       mtimeh;        
+        uint32_t                bitrate;       // Used by midgetv_blast/free
+        const volatile uint32_t minstreth;     // Write adr is different
+        volatile uint32_t       mcycleh;       
+        uint32_t                freefornow_f4;
+        const volatile uint32_t mtimecmp;      // Write adr is different
+        const volatile uint32_t mtimecmph;     // Write adr is different
+        volatile uint32_t       CSRretadr;     // 
+        void (*Entry_xRET)(void);    
+        void (*Entry_CSR )(void);    
+} SYSEBR_TypeDef;
+
+typedef struct {
+        volatile uint32_t D;
+} UART_TypeDef;
+
+/* A note to self:
+   Even though the leds are output registers, they must be declared volatile.
+   It turned out that the following othwise fails:
+   int main( void ) {
+           while ( 1 )
+                   LED->D = 5;
+   } 
+
+   Was optimized to:
+   main:
+           j main
+ */
+typedef struct {
+        volatile uint32_t D;
+} LED_TypeDef;
+
+typedef struct {
+        volatile uint32_t D; // Data
+        volatile uint32_t S; // Status
+} EPP_TypeDef;
+        
+#define EBRBASE  0x00000000
+#define IOBASE   0x60000000
+#define SRAMBASE 0x80000000
+
+#define SYS_BASE  (EBRBASE+0x00000080u)
+#define LED_BASE  (IOBASE +0x00000004u)
+#define UART_BASE (IOBASE +0x00000008u) 
+#define EPP_BASE  (IOBASE +0x00000008u) 
+
+/* Attempts to aliase SYSEBR_TypeDef to address 0 lead to desasters,
+   mixup with NULL in GCC suspected. */
+#define SYSEBR ((SYSEBR_TypeDef *)SYS_BASE )
+#define UART   ((UART_TypeDef *)  UART_BASE)
+#define EPP    ((EPP_TypeDef *)   EPP_BASE)
+#define LED    ((LED_TypeDef *)   LED_BASE )
+
+
