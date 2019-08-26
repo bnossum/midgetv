4b88c1b838ee62ba897e3231a4cc74442bcefe41
bnossum <bnossum@gmail.com>
2019-08-26 11:08:43 +0200

    Moved bitbang uart rx pin to bit 7

--- hwtst/upduino2/upduino2.v
+++ hwtst/upduino2/upduino2.v
@@ -208,7 +208,7 @@ module top
       .RST_I                            (1'b0),
       .meip                             (1'b0),
       .start                            (1'b1),
-      .DAT_I                            ({31'h0,meta_usartRX}),
+      .DAT_I                            ({24'h0,meta_usartRX,7'h0}),
       /*AUTOINST*/
       // Outputs
       .CYC_O                            (CYC_O),
