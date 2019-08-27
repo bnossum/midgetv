8d946ebce1d2b80dc9a6f51ddbf9abc961cfd0c5
bnossum <bnossum@gmail.com>
2019-08-27 06:29:56 +0200

    Minor changes

--- sw/dynhw/dynblinky/dynblinky.c
+++ sw/dynhw/dynblinky/dynblinky.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include "midgetv.h"
 
-// hexdump -ve '"%4.4_ax  " 4/4 "%8.8x " 1/0 "\n"'  file    // Usefull command
-//
-
-void cywait(uint32_t lim) {
+// -----------------------------------------------------------------------------
+static void cywait(const uint32_t lim) {
         volatile uint32_t i;
 
         for ( i = 0; i < lim; i++ )
                 ;
 }
 
+// -----------------------------------------------------------------------------
 int main( void ) {
         int i = 0;
         
