36e3dd6fb35b73f5d040ac0b20a10b145d2bd0f4
bnossum <bnossum@gmail.com>
2020-08-26 10:56:45 +0200

    m

--- work/code/simcode/m_ice40sim_EBRonly.v
+++ work/code/simcode/m_ice40sim_EBRonly.v
@@ -9,7 +9,7 @@
 module m_ice40sim_EBRonly
   # ( parameter
       SRAMADRWIDTH       = 0,  
-      SIMEBRADRWIDTH     = 9,  // MUST match corresponding macro in m_ice4sim_EBRonly.cpp
+      SIMEBRADRWIDTH     = 9,  // Corresponding macro in m_ice4sim_EBRonly.cpp MUST match this.
       IWIDTH             = 32, 
       NO_CYCLECNT        = 1, 
       MTIMETAP           = 14, 
