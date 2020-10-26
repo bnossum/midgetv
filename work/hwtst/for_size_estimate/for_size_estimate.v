0aedc1f62c61e9748756e451552651b6f61191fd
bnossum <bnossum@gmail.com>
2020-10-26 08:16:35 +0100

    m

--- work/hwtst/for_size_estimate/for_size_estimate.v
+++ work/hwtst/for_size_estimate/for_size_estimate.v
@@ -7,36 +7,7 @@
  * This file only to get an estimate of the size of the core.
  */
 
-`include "../../code/m_ice_shortcuts.v"
-`include "../../code/m_inputmux.v"
-`include "../../code/m_mimux.v"
-`include "../../code/m_alu_carryin.v"
-`include "../../code/m_alu.v"
-`include "../../code/m_immexp_zfind_q.v"
-`include "../../code/m_ram.v"
-`include "../../code/m_ram_a16.v"
-`include "../../code/m_ram_a17.v"
-`include "../../code/m_ebr.v"
-`include "../../code/m_ebr_w16.v"
-`include "../../code/m_ebr_w8.v"
-`include "../../code/m_ebr_w4.v"
-`include "../../code/m_ebr_w2.v"
-`include "../../code/m_rai.v"
-`include "../../code/m_wai.v"
-`include "../../code/m_opreg.v"
-`include "../../code/m_cyclecnt.v"
-`include "../../code/m_condcode.v"
-`include "../../code/m_shiftcounter.v"
-`include "../../code/m_status_and_interrupts.v"
-`include "../../code/m_ucode.v" 
-`include "../../code/m_3ebr.v"
-`include "../../generated/m_2ebr.v"
-`include "../../code/m_ucodepc.v"
-`include "../../code/m_progressctrl.v"
-`include "../../code/m_shlr.v"
-`include "../../code/m_RVC.v"
-`include "../../code/m_midgetv_core.v"
-
+`include "../../midgetv.v"
 
 /* Only define one of these:
  */
@@ -86,11 +57,9 @@ module mytop
 `ifdef INVESTIGATE_SMALLEST
    localparam IWIDTH                   = 1;
    localparam SRAMADRWIDTH             = 0;
-   localparam FORCEEBRADRWIDTH         = 8;
+   localparam FORCEEBRAWIDTH           = 10;
    localparam NO_CYCLECNT              = 1;
    localparam MTIMETAP                 = 0;
-   localparam LAZY_DECODE              = 2;
-   localparam DISREGARD_WB4_3_55       = 1;
    localparam DAT_I_ZERO_WHEN_INACTIVE = 1;  
    wire                RST_I = 1'b0;
    wire                start = 1'b1;
@@ -100,11 +69,9 @@ module mytop
 `ifdef INVESTIGATE_LARGEST
    localparam IWIDTH                   = 32;
    localparam SRAMADRWIDTH             = 17;
-   localparam FORCEEBRADRWIDTH         = 11;
+   localparam FORCEEBRAWIDTH           = 13;
    localparam NO_CYCLECNT              = 0;
    localparam MTIMETAP                 = 16;
-   localparam LAZY_DECODE              = 0;
-   localparam DISREGARD_WB4_3_55       = 0;
    localparam DAT_I_ZERO_WHEN_INACTIVE = 0;  
 
    /* To get this into a package I must reduce the number of IOs.
@@ -163,11 +130,9 @@ module mytop
 `ifdef GET_NICE_SYMBOL
    localparam IWIDTH             = 32;
    localparam SRAMADRWIDTH       = 17;
-   localparam FORCEEBRADRWIDTH   = 11;
+   localparam FORCEEBRAWIDTH     = 13;
    localparam NO_CYCLECNT        = 0;
    localparam MTIMETAP           = 16;
-   localparam LAZY_DECODE        = 0;
-   localparam DISREGARD_WB4_3_55 = 0;
 `endif
 
    
@@ -179,13 +144,11 @@ module mytop
 m_midgetv_core
      #(
        .SRAMADRWIDTH           ( SRAMADRWIDTH             ),
-       .EBRADRWIDTH            ( FORCEEBRADRWIDTH         ),
+       .EBRAWIDTH              ( FORCEEBRAWIDTH           ),
        .IWIDTH                 ( IWIDTH                   ),
        .NO_CYCLECNT            ( NO_CYCLECNT              ),
        .MTIMETAP               ( MTIMETAP                 ),
        .HIGHLEVEL              ( HIGHLEVEL                ),
-       .LAZY_DECODE            ( LAZY_DECODE              ),
-       .DISREGARD_WB4_3_55     ( DISREGARD_WB4_3_55       ),
       .DAT_I_ZERO_WHEN_INACTIVE( DAT_I_ZERO_WHEN_INACTIVE ),
        .prg00(prg00),       .prg01(prg01),       .prg02(prg02),       .prg03(prg03),
        .prg04(prg04),       .prg05(prg05),       .prg06(prg06),       .prg07(prg07),
@@ -214,95 +177,6 @@ m_midgetv_core
       .start                            (start));
 endmodule
 
-
-    // `ifdef FOR_SYMBOL
-    //    input              RST_I,
-    //    input              start,
-    //    input              meip,
-    // `endif
-    //`else
-    //    input              fakeDAT_I,
-    //    output             fake,
-    //`endif    
-   //`ifndef FOR_SYMBOL
-//`endif
-
-   
-//
-//  # ( parameter
-//`ifdef SMALLEST      
-//      SRAMADRWIDTH       = 0, 
-//      FORCEEBRADRWIDTH   = 8,
-//`ifdef FOR_SYMBOL      
-//      IWIDTH             = 32,
-//`else
-//      IWIDTH             = 8,
-//`endif
-//      NO_CYCLECNT        = 1, 
-//      MTIMETAP           = 0, 
-//      HIGHLEVEL          = 0, 
-//      LAZY_DECODE        = 2, 
-//      DISREGARD_WB4_3_55 = 1  
-//`else
-//      SRAMADRWIDTH       = 17,
-//      FORCEEBRADRWIDTH   = 11,
-//      IWIDTH             = 32, 
-//      NO_CYCLECNT        = 0, 
-//      MTIMETAP           = 16, 
-//      HIGHLEVEL          = 0,
-//      LAZY_DECODE        = 0,
-//      DISREGARD_WB4_3_55 = 0
-//`endif
-//      )
-//   
-//`ifndef SMALLEST
-//   /* To compile reduce pincount
-//    * So here we add artificial logic on output to keep results,
-//    * and also expands a 1-bit input to a DAT_I[31:0] input
-//    * All this to give an estimate of the size of the core.
-//    * From the number output from syntesis we must subtract:
-//    * 32   Shift register
-//    *  8   XOR on ADR_O
-//    *  8   XOR on DAT_O
-//    *  1   XOR on SEL_O
-//    *  4   XORs on the XOR of ADR_O and DAT_O
-//    *  1   XOR of line above
-//    *  1   XOR of line above and (XOR on SEL_O)
-//    * -----------
-//    * 55 luts in total
-//    * 
-//    * With ICECube2, we seem to have the result 466-55 = 411.
-//    * Obviously, midgetv does not live alone, but this is
-//    * the size of m_midgetv_core in the design.
-//    */
-//   
-//   reg [IWIDTH-1:0]    DAT_I;   
-//   wire [31:0]         ADR_O;
-//   wire [31:0]         DAT_O;
-//   wire [3:0]          SEL_O;
-//   wire [7:0]          xoradr;
-//   wire [7:0]          xordat;
-//   wire                xorsel;
-//   wire [1:0]          l2xoradr;
-//   wire [1:0]          l2xordat;
-//   wire                keep64;
-//
-//   always @(posedge CLK_I)
-//     DAT_I <= {DAT_I[IWIDTH-2:0],fakeDAT_I };
-//   
-//   assign xoradr = 
-//   assign xordat = DAT_O[31:24] ^ DAT_O[23:16] ^ DAT_O[15:8] ^ DAT_O[7:0];
-//   assign xorsel = ^SEL_O;
-//
-//
-//   assign l2xoradr[0] = &xoradr[3:0];
-//   assign l2xoradr[1] = &xoradr[7:4];
-//   assign l2xordat[0] = &xordat[7:4];
-//   assign l2xordat[1] = &xordat[7:4];
-//   
-//   assign keep64 = &l2xoradr | &l2xordat;
-//   assign fake = keep64 ^ xorsel;
-//`endif
 
       
 // Local Variables:
